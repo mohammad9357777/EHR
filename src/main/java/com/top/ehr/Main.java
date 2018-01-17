038217568a8af0aec599aacdd1cab5cb78e1697a
mohammad9357777 <35492442+mohammad9357777@users.noreply.github.com>
2018-01-17 10:06:31 +0330

    Add files via upload

---
+++ src/main/java/com/top/ehr/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.top.ehr;
+
+/**
+ *
+ * @author Soft 1
+ */
+public class Main {
+    private String s="";
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
